1fa23052052b3d9ff86c64e597c588c89f3540b4
Pythagean <jeremysto@gmail.com>
2026-05-22 15:32:25 +1000

    Added new backgrounds for results cards. Slight tweak to categorie thresholds

--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -455,8 +455,8 @@ export default function ResultsPage({ results = [], guessesByPage = {}, onBack, 
                     const t = Number(total) && Number(total) >= 0 ? Number(total) : 0;
                     let category = 'Terrible';
                     if (t <= 6) category = 'Perfect';
-                    else if (t <= 9) category = 'Great';
-                    else if (t <= 13) category = 'Good';
+                    else if (t <= 8) category = 'Great';
+                    else if (t <= 12) category = 'Good';
                     else if (t <= 17) category = 'Okay';
                     else if (t <= 21) category = 'Bad';
                     else category = 'Terrible';
